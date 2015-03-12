093760159a6df20ea8c1b25e3e83ab4f1ace5361
Tom Robinson <tlrobinson@gmail.com>
2015-03-12 13:28:31 -0700

    Remove API key (and revoke it)

--- examples/example-script.php
+++ examples/example-script.php
@@ -6,8 +6,8 @@ require dirname(__FILE__) . '/../vendor/autoload.php';
 
 use Asana\Client;
 
-const ASANA_API_KEY = '5KuX5z.wySa0u7i9ag7tCVwLJktAbHvX';
+$ASANA_API_KEY = getenv("ASANA_API_KEY");
 
-$client = Client::basicAuth(ASANA_API_KEY);
+$client = Client::basicAuth($ASANA_API_KEY);
 
 var_dump($client->users->me()->workspaces);
