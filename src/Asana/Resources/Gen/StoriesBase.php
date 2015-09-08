14122a912da2bb1297b19e5db69eda687a2d2be3
Andrew Noonan <andrew@noonan.cc>
2015-09-08 16:08:16 -0700

    Deploy from asana-api-meta v0.4.5 (c7c1b64/master) by noonhub

--- src/Asana/Resources/Gen/StoriesBase.php
+++ src/Asana/Resources/Gen/StoriesBase.php
@@ -9,16 +9,16 @@ class StoriesBase
         $this->client = $client;
     }
 
-    public function findById($story, $params = array(), $options = array())
-    {
-        $path = sprintf("/stories/%s", $story);
-        return $this->client->get($path, $params, $options);
-    }
-
     public function findByTask($task, $params = array(), $options = array())
     {
         $path = sprintf("/tasks/%s/stories", $task);
         return $this->client->getCollection($path, $params, $options);
+    }
+
+    public function findById($story, $params = array(), $options = array())
+    {
+        $path = sprintf("/stories/%s", $story);
+        return $this->client->get($path, $params, $options);
     }
 
     public function createOnTask($task, $params = array(), $options = array())
