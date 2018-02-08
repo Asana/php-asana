4c04419cdeb29591b4e1d2c7908460ff62534d61
Johnny Goodnow <jgoodnow@fastmail.com>
2018-02-07 23:50:57 -0800

    Handle 402/403's with current API setup.

--- src/Asana/Errors/AsanaError.php
+++ src/Asana/Errors/AsanaError.php
@@ -12,6 +12,7 @@ use Asana\Errors\ServerError;
 
 class AsanaError extends \Exception
 {
+    const PREMIUM_ONLY_STR = "not available for free";
     public function __construct($message, $status, $response)
     {
         $this->message = $message;
@@ -23,6 +24,16 @@ class AsanaError extends \Exception
     {
         switch ($response->code) {
             case ForbiddenError::STATUS:
+                // Handle 403's with a premium response
+                try {
+                    foreach ($response->body->errors as $error) {
+                        if (strpos($error->message, self::PREMIUM_ONLY_STR) !== false) {
+                            throw new PremiumOnlyError($response);
+                        }
+                    }
+                } catch (Exception $e) {
+                    throw new ForbiddenError($response);
+                }
                 throw new ForbiddenError($response);
             case InvalidRequestError::STATUS:
                 throw new InvalidRequestError($response);
@@ -32,6 +43,8 @@ class AsanaError extends \Exception
                 throw new NoAuthorizationError($response);
             case NotFoundError::STATUS:
                 throw new NotFoundError($response);
+            case PremiumOnlyError::STATUS:
+                throw new PremiumOnlyError($response);
             case RateLimitEnforcedError::STATUS:
                 throw new RateLimitEnforcedError($response);
             case ServerError::STATUS:
